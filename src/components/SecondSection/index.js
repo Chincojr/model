6871cd1690ac614a5b51ffaa574db2f77d3fec2b
chincojr <akindeindedavid@gmail.com>
2023-10-06 17:52:02 +0100

    Initial commit

---
+++ src/components/SecondSection/index.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import './index.css'
+import {MdOutlineKeyboardArrowRight} from 'react-icons/md'
+
+const Index = ({handleChange}) => {
+  return (
+    <>
+    
+    <div className='flex flex-col w-full h-full justify-center items-center '>
+        <h1 className='text-[30px] text-center lg:text-[65px] font-semibold mb-[40px]'>
+            Where do you live in:
+        </h1>
+        <div className='grid w-full grid-rows-3 lg:grid-rows-1 lg:grid-cols-3 gap-5 h-full'>
+            <div className='second-section-selection'> 
+                <div className='selection-top'>
+                    <img alt="house" className="second-selection-img" src='https://cbvalueaddrealty.in/wp-content/uploads/2022/07/condominium.jpg' />
+                    <h4 className='image-info'>Amelie Townsend <br /><span>( I live in a townhome.)</span></h4>
+                </div>
+                <div className='selection-bottom'>
+                    <div className='selection-caption-container '>
+                       <button onClick={() => handleChange('building','Condo/Townhome')} className='selection-caption'>Condo/Townhome</button>
+                       <div className='h-[70%] mt-[2px] flex justify-center items-center'>
+                            <MdOutlineKeyboardArrowRight className=''  />
+                       </div>
+                    </div>
+                </div>
+            </div>            
+            <div className='second-section-selection'> 
+                <div className='selection-top'>
+                    <img alt="house" className="second-selection-img" src='https://www.southernliving.com/thmb/5EvDoCH-TH-oqKicpHE4wWaNKtQ=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/27196_AdaptiveCottage00006-2000-7702094217044ce4830a8adb0a69b6da.jpg'/>
+                    <h4 className='image-info'>Amelie Townsend <br /><span>( I live in a townhome.)</span></h4>
+                </div>
+                <div className='selection-bottom'>
+                <div className='selection-caption-container hover:bg[#1a1d21]'>
+                       <button onClick={() => handleChange('building','Single Story Home')} className='selection-caption'>Single Story Home</button>
+                       <div className='h-[70%] mt-[2px] flex justify-center items-center'>
+                            <MdOutlineKeyboardArrowRight className=''  />
+                       </div>
+                    </div>
+                </div>
+            </div>
+            <div className='second-section-selection'> 
+                <div className='selection-top'>
+                    <img alt="house" className="second-selection-img" src='https://www.thehousedesigners.com/images/uploads/SiteImage-Landing-two-story-house-plans-1.webp' />
+                    <h4 className='image-info'>Amelie Townsend <br /><span>( I live in a townhome.)</span></h4>
+                </div>
+                <div className='selection-bottom'>
+                <div className='selection-caption-container hover:bg[#1a1d21]'>
+                        <button onClick={() => handleChange('building','Multi Story Home')} className='selection-caption'>Multi Story Home</button>
+                       <div className='h-[70%] mt-[2px] flex justify-center items-center'>
+                            <MdOutlineKeyboardArrowRight className=''  />
+                       </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+     </div>
+    
+    </>
+  )
+}
+
+export default Index
---
+++ src/components/ThirdSection/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './index.css'
+
+
+const Index = ({handleChange, model}) => {
+  return (
+    <>
+     <div className='flex flex-col w-full h-full justify-center items-center '>
+        <h1 className='text-[30px] text-center lg:text-[65px] font-semibold mb-[40px]'>
+            The purpose of this project is:
+        </h1>
+        <div className='grid w-full grid-rows-2 lg:grid-rows-1 lg:grid-cols-2 gap-10 lg:gap-1'>
+            <div className='third-section-left' >
+                <img alt='' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0sRNjb7ls6ZzJf6LpZZh2qYnExNYvUF8zF_kMqHIa70d96DyojuUkJtfN_V1DNQULdOE&usqp=CAU' className='third-section-img'/>
+                <div className='third-selection-caption-container'>
+                    <button onClick={() => handleChange('type','complete')} className='selection-caption-third'>Complete {model} Remodel</button>
+                </div>
+
+            </div>
+            <div className='third-section-right' >
+                <div className='third-section-left-img-container'>
+                    <img alt='' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbDPz7_AWVqJPNh8GOUbHq3HkrJ4S1jz8a5cTWJ_e8KxCgD0MnWXP3rEYhZVBgijeiHEU&usqp=CAU' className='third-section-img'/>
+                    <img alt='' src='https://metrointeriors.com/wp-content/uploads/sites/7/2019/08/IMG_3510-270x180.jpeg' className='third-section-img'/>
+                </div>
+                <div className='third-selection-caption-container'>
+                    <button onClick={() => handleChange('type','partial')}   className='selection-caption-third'>Partial {model} Remodel</button>
+                </div>            
+            </div>
+        </div>
+     </div>
+    </>
+  )
+}
+
+export default Index
